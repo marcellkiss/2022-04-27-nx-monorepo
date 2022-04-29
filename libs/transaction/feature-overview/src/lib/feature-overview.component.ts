597a2bc173ffa2ef60ee9671626f2f89910db458
Marcell Kiss <kissmarcell88@gmail.com>
2022-04-29 14:16:56 +0200

    feat: add routing to feature-overview in transaction to check lazy loaded js

--- libs/transaction/feature-overview/src/lib/feature-overview.component.ts
+++ libs/transaction/feature-overview/src/lib/feature-overview.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-name',
-  template: `FEATURE OVERVIEW <bp-button></bp-button> <br/> <bp-button-group></bp-button-group>`,
+  template: `FEATURE OVERVIEW <bp-button></bp-button> <br />
+    <bp-button-group></bp-button-group>
+    <br />
+    <br />
+    <a [routerLink]="['/report']">Go To Reports</a>`,
   styles: [``],
 })
 export class FeatureOverviewComponent {}
--- libs/transaction/feature-overview/src/lib/feature-overview.module.ts
+++ libs/transaction/feature-overview/src/lib/feature-overview.module.ts
@@ -1,17 +1,14 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { AtomsModule } from '@blockpit-nx-example/design-system/atoms';
-import { FeatureOverviewComponent } from './feature-overview.component';
 import { MoleculesModule } from '@blockpit-nx-example/design-system/molecules';
+import { FeatureOverviewComponent } from './feature-overview.component';
 
 @NgModule({
-    declarations: [ FeatureOverviewComponent ],
-    imports: [
-        CommonModule,
-        AtomsModule,
-        MoleculesModule
-    ],
-    exports: [ FeatureOverviewComponent ],
-    providers: [],
+  declarations: [FeatureOverviewComponent],
+  imports: [CommonModule, AtomsModule, MoleculesModule, RouterModule],
+  exports: [FeatureOverviewComponent],
+  providers: [],
 })
 export class FeatureOverviewModule {}
